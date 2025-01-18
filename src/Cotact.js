6af1a57466546aee5cc0009b04f2a00ec2429f49
AyaanSidyt <142896428+AyaanSidyt@users.noreply.github.com>
2025-01-18 21:37:11 +0500

    Update Cotact.js

--- src/Cotact.js
+++ src/Cotact.js
@@ -8,7 +8,7 @@ function Contact() {
         <div className="contact-info">
           <h2>Contact Us</h2>
           <p><strong>Phone:</strong> +92 321-9247986</p>
-          <p><strong>Email:</strong> contact@example.com</p>
+          <p><strong>Email:</strong>tahseen.ahmed58@gmail.com</p>
         </div>
       </div>
     </>
